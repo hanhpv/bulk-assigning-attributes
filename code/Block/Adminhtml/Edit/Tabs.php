e1e09d24cfaec1a9c8da9c01ca1233f44dae7c79
Hanh Phung Van <hanhpv@dtn.com.vn>
2015-09-15 07:08:05 +0000

    Override edit attribute tabs

---
+++ code/Block/Adminhtml/Edit/Tabs.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Override Mage_Adminhtml_Block_Catalog_Product_Attribute_Edit_Tabs
+*/
+class Dtn_Eav_Block_Adminhtml_Attribute_Edit_Tabs extends Mage_Adminhtml_Block_Catalog_Product_Attribute_Edit_Tabs
+{
+	
+	protected function _beforeToHtml()
+    {
+        $this->addTab('attribute_sets', array(
+            'label'     => Mage::helper('catalog')->__('Attribute Sets'),
+            'title'     => Mage::helper('catalog')->__('Attribute Sets'),
+            'class'     => 'ajax',
+			'url'       => $this->getUrl('*/approval/pending', array('_current' => true)),
+            'after'		=> 'labels'
+        ));
+
+        return parent::_beforeToHtml();
+    }
+}
